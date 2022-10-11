390ac2d98c4bdbada352c2431e688277c2279a34
Max Kottong <max.kottong@gmail.com>
2022-10-11 13:38:25 -0400

    Finishing of tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import { render, cleanup, getByLabelText } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    it('renders', () => {
+        render(<Nav />);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        const { getByLabelText } = render(<Nav />);
+
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+});
+
+describe('links are visibnle', () => {
+    it('inserts text into the links', () => {
+        const { getByTestId } = render(<Nav />);
+
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+});
